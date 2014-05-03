7cdef09bd8a11c722907605f46b57e92f9c11c05
Benoit Rajalu <rajalubenoit@gmail.com>
2014-05-03 23:52:52 +0200

    Looking at it another way

--- source/js/plugins.js
+++ source/js/plugins.js
@@ -27,7 +27,7 @@ if (!(window.console && console.log)) {
 			var sendTo = $("#contactForm").attr("action");
 			console.log(sendTo);
 			$.ajax({
-			  type: "POST",
+			  type: "GET",
 			  url: sendTo,
 			  data: $("#contactForm").serialize(),
 			  success: function() {
